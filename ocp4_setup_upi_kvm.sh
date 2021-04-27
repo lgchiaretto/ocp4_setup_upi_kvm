4baaa251b89feb67de7982d60e37027cd2984ce1
Luiz Chiaretto <luizgustavo@chiaretto.com.br>
2021-04-27 14:44:31 -0300

    added all nodes on haproxy and changed cluster directory

--- ocp4_setup_upi_kvm.sh
+++ ocp4_setup_upi_kvm.sh
@@ -192,7 +192,7 @@ test -z "$CLUSTER_NAME" && CLUSTER_NAME="ocp4"
 test -z "$BASE_DOM" && BASE_DOM="local"
 test -z "$DNS_DIR" && DNS_DIR="/etc/NetworkManager/dnsmasq.d"
 test -z "$VM_DIR" && VM_DIR="/var/lib/libvirt/images"
-test -z "$SETUP_DIR" && SETUP_DIR="/root/ocp4_setup_${CLUSTER_NAME}"
+test -z "$SETUP_DIR" && SETUP_DIR="/vms/clusters/ocp4_setup_${CLUSTER_NAME}"
 test -z "$CACHE_DIR" && CACHE_DIR="/root/ocp4_downloads" && mkdir -p "$CACHE_DIR"
 test -z "$PULL_SEC_F" && PULL_SEC_F="/root/pull-secret"; PULL_SEC=$(cat "$PULL_SEC_F")
 
@@ -793,6 +793,10 @@ do
     echo "  server master-${i} master-${i}.${CLUSTER_NAME}.${BASE_DOM}:443 check" >> haproxy.cfg
 done
 
+for i in $(seq 1 ${N_WORK})
+do
+    echo "  server worker-${i} worker-${i}.${CLUSTER_NAME}.${BASE_DOM}:443 check" >> haproxy.cfg
+done
 
 
 
